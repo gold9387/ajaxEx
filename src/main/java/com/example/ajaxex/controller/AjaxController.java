c640ca727cff895cb15619ba135d5c00cf754e81
gold9387 <gold9387@gmail.com>
2023-05-11 18:22:01 +0900

    update

--- src/main/java/com/example/ajaxex/controller/AjaxController.java
+++ src/main/java/com/example/ajaxex/controller/AjaxController.java
@@ -1,8 +1,13 @@
 package com.example.ajaxex.controller;
 
 import com.example.ajaxex.dto.AjaxDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class AjaxController {
@@ -34,9 +39,50 @@ public class AjaxController {
     }
 
     @GetMapping("/ex05")
-    public @ResponseBody String ex05(@ModelAttribute AjaxDTO ajaxDTO) {
-        System.out.println("ajecDTO = " + ajaxDTO);
-        return "ex05 메서드 호출 완료";
+    public @ResponseBody AjaxDTO ex05(@ModelAttribute AjaxDTO ajaxDTO) {
+        System.out.println("ajaxDTO = " + ajaxDTO);
+        return ajaxDTO;
+    }
 
+    @PostMapping("/ex06")
+    public @ResponseBody AjaxDTO ex06(@ModelAttribute AjaxDTO ajaxDTO) {
+        System.out.println("ajaxDTO = " + ajaxDTO);
+        return ajaxDTO;
+    }
+
+    @PostMapping("/ex07")
+    public @ResponseBody AjaxDTO ex07(@RequestBody AjaxDTO ajaxDTO) {
+        System.out.println("ajaxDTO = " + ajaxDTO);
+        return ajaxDTO;
+    }
+
+    private List<AjaxDTO> DTOList() {
+        List<AjaxDTO> dtoList = new ArrayList<>();
+        dtoList.add(new AjaxDTO("data1", "data11"));
+        dtoList.add(new AjaxDTO("data2", "data22"));
+        return dtoList;
+    }
+
+    @PostMapping("/ex08")
+    public @ResponseBody List<AjaxDTO> ex08(@RequestBody AjaxDTO ajaxDTO) {
+        System.out.println("ajaxDTO = " + ajaxDTO);
+        List<AjaxDTO> dtoList = DTOList();
+        dtoList.add(ajaxDTO);
+        return dtoList;
+    }
+
+    @PostMapping("/ex09")
+    public ResponseEntity ex09(@RequestBody AjaxDTO ajaxDTO) {
+        System.out.println("ajaxDTO = " + ajaxDTO);
+//        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(ajaxDTO, HttpStatus.OK);
+    }
+
+    @PostMapping("/ex10")
+    public ResponseEntity ex10(@RequestBody AjaxDTO ajaxDTO) {
+        System.out.println("ajaxDTO = " + ajaxDTO);
+        List<AjaxDTO> dtoList = DTOList();
+        dtoList.add(ajaxDTO);
+        return new ResponseEntity<>(dtoList, HttpStatus.OK);
     }
 }
